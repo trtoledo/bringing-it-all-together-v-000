8c9e214df14a8ffd108a6a5848927a6047b1af0e
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-06 22:00:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,7 +71,7 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      dog = Song.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
       dog = self.create(id: nil, name: name, breed: breed)
     end
