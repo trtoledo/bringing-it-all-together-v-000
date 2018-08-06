1487dbafd30e421f14608dfc903044abbf09f6f9
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-06 22:14:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,6 +78,17 @@ class Dog
     dog
   end
 
+  def self.new_from_db
+
+
+
+  end
+
+  def self.find_by_name
+
+
+  end
+
   # def self.find_or_create_by(name:, breed:)
   #   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   #   if !dog.empty?
