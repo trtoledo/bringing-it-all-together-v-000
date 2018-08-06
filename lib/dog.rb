8741025028b3ae67be70d0e7bb9ab583c9b27e87
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-06 21:54:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,8 +69,8 @@ class Dog
 
 
   def update
-    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.id)
+    sql = "UPDATE songs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
 
   end
 
